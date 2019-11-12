09a84a63b9f4adf0efa91b85418dc0ae598b8975
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-12 22:17:09 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bonus — Collections Practice
+ # Bonus — Collections Practice
 
 ## Objectives
 
--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort.reverse 
+end 
+
+def sort_array_char_count(array)
+  array.sort_by {|string| string.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse 
+end 
+
+def kesha_maker(array)
+  array.each 
